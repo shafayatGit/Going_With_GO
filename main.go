bc16fb00958f53811fbed61bc344e6a7be833cf2
shafayatGit <shafayathossain.ielts@gmail.com>
2026-06-22 17:41:30 +0600

    variable declarations

--- main.go
+++ main.go
@@ -2,7 +2,34 @@ package main
 
 import "fmt" //standard library
 
-func main(){
-	fmt.Println("Hello World!")
+var isAdmin bool = true
+
+// isAdmin:=true ==> will not work
+
+func main() {
+	// var name string;
+	// name = "King"
+	// var name string = "Shafayat"
+	name := "Shafayat" // Go took type by its own => Short Way and mostly used but will not work outside of the function
+	// name string :="king" //cant do this
+
+	//Variable Grouping
+	var (
+		age         int    = 23
+		email       string = "king@gmai"
+		isAvailable bool   = true
+	)
+
+	// => Multiple variable declaration
+	// var x,y int;
+	// x=10
+	// y=20
+
+	const pi = 3.1416 // constant and never going to change and cant re-asign
+
+	var x, y int = 20, 30
+	fmt.Println("Hello World!", name, age, email, isAvailable)
+	fmt.Println("Hello World!", x, y)
 }
+
 //no need to call main file/function
