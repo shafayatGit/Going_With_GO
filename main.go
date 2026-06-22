61849bf3d49d4364695cef141eaea6cd324a0576
shafayatGit <shafayathossain.ielts@gmail.com>
2026-06-22 20:11:20 +0600

    function

--- main.go
+++ main.go
@@ -6,6 +6,32 @@ var isAdmin bool = true
 
 // isAdmin:=true ==> will not work
 
+//------------>Without Return function
+// func makeCoffee(kind string) {
+// 	fmt.Printf("Making %s coffee", kind)
+// }
+
+//-------------> Return
+// func makeCoffee(kind string) string {
+// 	fmt.Printf("Making %s coffee \n", kind)
+// 	return kind
+// }
+
+// --------->Multiple return value
+//
+//	func makeCoffee(kind string) (string, int) {
+//		price := 100
+//		fmt.Printf("Making %s coffee \n", kind)
+//		return kind, price
+//	}
+
+// -------> Multiple named return
+func makeCoffee(kind string) (coffee string, price int) {
+	price = price
+	coffee = coffee
+	fmt.Printf("Making %s coffee \n", kind)
+	return // no need to tell anything
+}
 func main() {
 	// var name string;
 	// name = "King"
@@ -30,6 +56,9 @@ func main() {
 	var x, y int = 20, 30
 	fmt.Println("Hello World!", name, age, email, isAvailable)
 	fmt.Println("Hello World!", x, y)
+
+	myCoffee, myBill := makeCoffee("Black")
+	fmt.Println(myCoffee, myBill)
 }
 
 //no need to call main file/function
