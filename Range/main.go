90fa6df5c9b7359b386c06dd76d5c058cb10ba30
shafayatGit <shafayathossain.ielts@gmail.com>
2026-06-23 15:51:36 +0600

    Range

---
+++ Range/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+	// myMap := map[string]string{
+	// 	"Name":    "Mizan",
+	// 	"Success": "ok",
+	// }
+
+	// for _, value := range myMap {
+	// 	fmt.Println(value)
+	// }
+
+	// myArr := [3]string{
+	// 	"green",
+	// 	"red",
+	// 	"black",
+	// }
+
+	// colors := []string{
+	// 	"green",
+	// 	"red",
+	// 	"black",
+	// }
+
+	// for i, color := range colors {
+	// 	fmt.Println(i, color)
+	// }
+
+	name := "NextLevel"
+
+	var byteSlice = []byte(name)
+
+	fmt.Println(byteSlice)
+
+	// for i, value := range name {
+	// 	fmt.Println(i, value)
+	// }
+
+}
+
+// array
+// slice
+// string
+// map
+// channel
